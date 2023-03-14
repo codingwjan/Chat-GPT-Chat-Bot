e88b7fe8ac0111ce6f82aa127ad07bc094000471
jan pink <02-schablone.depots@icloud.com>
2023-03-14 06:34:56 +0100

    chore: working demo

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 def openai(messages):
     import openai
-    openai.api_key = "sk-xxxxxxxxxxxxxxxxxxxxx"
+    openai.api_key = "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"
 
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-0301",
-        temperature=0.9,
+        temperature=1,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0.6,
@@ -15,8 +15,10 @@ def openai(messages):
 
 
 def chat():
-    print("Hello, I'm a chatbot")
-    messages = []
+    print("Welcome to the OpenAI GPT-3 chatbot!")
+    messages = [
+        {"role": 'system', "content":"You are a Python developer that also has other interests."}
+    ]
 
     while True:
         text = input("You: ")
