d723bcbfbdf5119dc26db8599447fcbe6d7d550f
jan pink <02-schablone.depots@icloud.com>
2023-03-14 05:45:17 +0100

    chore: updated answer

--- src/main.py
+++ src/main.py
@@ -5,16 +5,13 @@ def openai(messages):
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-0301",
         temperature=0.9,
-        max_tokens=150,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0.6,
         messages=messages
     )
-    if completion.choices[0].message.content.find("\n") != -1:
-        return (completion.choices[0].message.content.replace("\n", ""))
-    else:
-        return (completion.choices[0].message.content)
+    messages.append({"role": "assistant", "content": completion.choices[0].message.content})
+    return (completion.choices[0].message.content)
 
 
 def chat():
