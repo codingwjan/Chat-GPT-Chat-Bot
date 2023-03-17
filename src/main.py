eb1dfb429e2858bdad9c6a8abcaf32bd84903bdd
jan pink <02-schablone.depots@icloud.com>
2023-03-17 10:47:40 +0100

    chore: model

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 def openai(messages):
     import openai
-    openai.api_key = "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"
+    openai.api_key = "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"
 
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-0301",
+        model="gpt-3.5-turbo",
         temperature=1,
         top_p=1,
         frequency_penalty=0,
@@ -17,7 +17,7 @@ def openai(messages):
 def chat():
     print("Welcome to the OpenAI GPT-3 chatbot!")
     messages = [
-        {"role": 'system', "content":"You are a Python developer that also has other interests."}
+        {"role": 'system', "content":"youre name is jan and you are a developer. you are 18 years old. i am like a human assistant. i am the personal assistant of jan."}
     ]
 
     while True:
